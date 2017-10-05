5ea41cd26b0a5964a32600cd258c46f6b551856a
Marcin <31159047+LazyAdminBlog@users.noreply.github.com>
2017-10-05 15:16:12 +0200

    Create main.cpp

---
+++ latwe/00005 - 496 - Dwie cyfry silni/CPlusPlus/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int i=0,j=0;
+    unsigned long long int a=0;
+    cin>>i;
+    for(j;j<i;++j)
+    {
+        cin>>a;
+        switch(a)
+        {
+        case 0:
+        case 1:
+            cout<<"0 1"<<endl;
+            break;
+        case 2:
+            cout<<"0 2"<<endl;
+            break;
+        case 3:
+            cout<<"0 6"<<endl;
+            break;
+        case 4:
+            cout<<"2 4"<<endl;
+            break;
+        case 5:
+        case 6:
+        case 8:
+            cout<<"2 0"<<endl;
+            break;
+        case 7:
+            cout<<"4 0"<<endl;
+            break;
+        case 9:
+            cout<<"8 0"<<endl;
+            break;
+        default:
+            cout<<"0 0"<<endl;
+            break;
+        }
+    }
+    return 0;
+}
