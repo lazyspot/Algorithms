59b115cb8c98b16115fa1877b1e532b82830e18c
Marcin <31159047+LazyAdminBlog@users.noreply.github.com>
2017-10-05 15:17:54 +0200

    Create PYTHON3.py

---
+++ latwe/00005 - 496 - Dwie cyfry silni/Python 3/PYTHON3.py
@@ -0,0 +1,25 @@
+standard_input = int(input(''))
+for i in range(0, standard_input, +1):
+    number = int(input(''))
+    if number == 1:
+        print ('0 1')
+    elif number == 0:
+        print ('0 1')
+    elif number == 2:
+        print ('0 2')
+    elif number == 3:
+        print ('0 6')
+    elif number == 4:
+        print ('2 4')
+    elif number == 5:
+        print ('2 0')
+    elif number == 6:
+        print ('2 0')
+    elif number == 7:
+        print ('4 0')
+    elif number == 8:
+        print  ('2 0')
+    elif number == 9:
+        print ('8 0')
+    else:
+        print ('0 0')
