1ddcbb43fb2ec65184c7fcb0adb05d213f4c5150
Marcin <31159047+LazyAdminBlog@users.noreply.github.com>
2017-10-08 16:27:29 +0200

    Create main.c

---
+++ latwe/00009 - 968 - Suma/C/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j=0, k;
+    while(scanf("%d", &i) != EOF)
+    {
+        j+=i;
+        printf("%d\n",j);
+    }
+    return 0;
+}
