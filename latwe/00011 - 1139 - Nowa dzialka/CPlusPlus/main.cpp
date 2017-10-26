802917eb216e330ecdbd3bb8b11ac960d71b7a94
Marcin <31159047+LazyAdminBlog@users.noreply.github.com>
2017-10-26 19:45:24 +0200

    Create main.cpp

---
+++ latwe/00011 - 1139 - Nowa dzialka/CPlusPlus/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int D,j=0;
+    unsigned long long int X=0;
+    cin>>D;
+    for(j;j<D;++j)
+    {
+        cin>>X;
+        cout<<X*X<<endl;
+    }
+    return 0;
+}
