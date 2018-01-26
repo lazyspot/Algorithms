e95a53783f92b1149cfe2d663d20ca29fcb477dc
Marcin <31159047+LazyAdminBlog@users.noreply.github.com>
2018-01-26 13:51:59 +0100

    Create main.cpp

---
+++ latwe/00024 - 506 - Flamaster/CPlusPlus/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int c, q, i;
+    char p;
+    char input[200];
+    cin>>c;
+    ++c;
+    while(--c)
+    {
+        cin>>input;
+        for(i=0;input[i]!='\0';++i)
+        {
+            if(input[i]==input[i+1] && input[i+1]==input[i+2])
+            {
+                p=input[i];
+                q=2;
+                while(input[i+q]==p)
+                {
+                    ++q;
+                }
+                cout<<input[i];
+                cout<<q;
+                i+=q;
+                --i;
+
+            }
+            else
+            {
+                cout<<input[i];
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
