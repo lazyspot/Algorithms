9ddd793ff00a9e20b61cf65e25e74d35c2b622ba
Marcin <31159047+LazyAdminBlog@users.noreply.github.com>
2018-01-26 19:06:00 +0100

    Create main.cpp

---
+++ latwe/00030 - 1042 - Transponowanie maierzy/CPlusPlus/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m, n, i, j;
+    cin>>m>>n;
+    int tab [200][200];
+    for(i=0;i<m;++i)
+    {
+        for(j=0;j<n;++j)
+        {
+            cin>>tab[i][j];
+        }
+    }
+    for(i=0;i<n;++i)
+    {
+        for(j=0;j<m;++j)
+        {
+            cout<<tab[j][i]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
