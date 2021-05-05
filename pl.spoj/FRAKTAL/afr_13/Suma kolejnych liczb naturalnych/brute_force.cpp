7072e767181d269145beacf93ef501951c1ba91a
Marcin <31159047+lazyspot@users.noreply.github.com>
2021-05-05 19:00:40 +0200

    Modify brute force for Suma kolejnych liczb naturalnych.

--- pl.spoj/FRAKTAL/afr_13/Suma kolejnych liczb naturalnych/brute_force.cpp
+++ pl.spoj/FRAKTAL/afr_13/Suma kolejnych liczb naturalnych/brute_force.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 int main()
 {
     long long length, sum, start = 1;
     cin >> length >> sum;
 
-    while((((a + length) / 2) * length) < sum){
+    while((2 * start + (length - 1)) * length / 2 < sum){
         start++;
     }
-
-    start--;
+ 
     for(int i = 0; i < length; i++){
         cout << start + i << " ";
-    }cout << endl;
+    }
+    cout << endl;
 
     return 0;
 }
